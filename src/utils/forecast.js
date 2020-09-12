19738beb9eead8241c7c68977b1091ba8a02ca15
daniel agbey <dannida>
2020-09-12 13:48:27 -0700

    Add time and visibility to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,27 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const weatherURL = 'http://api.weatherstack.com/current?access_key=eeddfddb6b35c8aa595c72be8b8bc90b&query=' + latitude + ',' + longitude + '&units=f'
+    const weatherURL =
+        "http://api.weatherstack.com/current?access_key=eeddfddb6b35c8aa595c72be8b8bc90b&query=" + latitude + "," + longitude + "&units=f";
     request({ url: weatherURL, json: true }, (error, response) => {
-
-        const tmp = response.body.current.temperature
-        const prec = response.body.current.precip
-        const weatherDescrip = response.body.current.weather_descriptions
+        const tmp = response.body.current.temperature;
+        const prec = response.body.current.precip;
+        const weatherDescrip = response.body.current.weather_descriptions;
+        const time = response.body.location.localtime;
+        const visibility = response.body.current.visibility;
 
         if (error) {
-            callback('Unable to connect to weather service! Check your Internet Connectivity', undefined)
+            callback(
+                "Unable to connect to weather service! Check your Internet Connectivity", undefined
+            );
         } else if (response.body.error) {
-            callback('Unable to find Location', undefined)
+            callback("Unable to find Location", undefined);
         } else {
-            callback(undefined, weatherDescrip + '. It is currently ' + tmp + ' degrees out there. ' + 'There is ' + prec + '% chance of rain')
+            callback(
+                undefined, weatherDescrip + ". It is currently " + tmp + " degrees out there. " + "There is " + prec + "% chance of rain." + "\r\n" + "Time of report: " + time + " Visibility is: " + visibility
+            );
         }
-    })
-}
+    });
+};
 
-module.exports = forecast
+module.exports = forecast;
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,22 +1,22 @@
-const request = require('request') 
+const request = require("request");
 
 const geocode = (address, callback) => {
-
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZGFubmlkYSIsImEiOiJja2RrNm5xY2YwbHk4MnhsY2E0dmk1MWJzIn0.6QR1dRmt5UeyQFh2hyEDLQ&limit=1'
+    const url =
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiZGFubmlkYSIsImEiOiJja2RrNm5xY2YwbHk4MnhsY2E0dmk1MWJzIn0.6QR1dRmt5UeyQFh2hyEDLQ&limit=1";
 
     request({ url: url, json: true }, (error, response) => {
         if (error) {
-            callback('Unable to connect to Location services', undefined)
+            callback("Unable to connect to Location services", undefined);
         } else if (response.body.features.length === 0) {
-            callback('Unable to find Location. Try another search', undefined)
+            callback("Unable to find Location. Try another search", undefined);
         } else {
             callback(undefined, {
                 latitude: response.body.features[0].center[1],
                 longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
-            })
+                location: response.body.features[0].place_name,
+            });
         }
-    })
-}
+    });
+};
 
-module.exports = geocode
+module.exports = geocode;
